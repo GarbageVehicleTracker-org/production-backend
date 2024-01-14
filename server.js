49ef7c3be8c4cb21dd4a7e0b6a493221acd6a880
Dhabaldeep <deepdhabal2001@gmail.com>
2024-01-14 22:34:20 +0530

    implementing the socket

--- server.js
+++ server.js
@@ -30,6 +30,15 @@ app.use(
   })
 );
 
+io.origins('https://municipality-garbage-tracking.onrender.com'); // Replace with your actual frontend URL
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://municipality-garbage-tracking.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 const port = process.env.PORT || 5500;
 
 connectToMongoDB();
