d125da1ac610c7c9aecdb522b1e82f42dd6d7322
Dhabaldeep <deepdhabal2001@gmail.com>
2024-05-15 02:00:14 +0530

    update the coordinates controller

--- controllers/Coordinates.controllers.js
+++ controllers/Coordinates.controllers.js
@@ -3,12 +3,14 @@ import { io } from "../server.js";
 
 class CoordinatesController {
   updateCoordinates({ latitude, longitude }) {
+    const { latitude, longitude } = req.body;
     try {
       if (!latitude || !longitude) {
         console.error("Latitude and longitude are required");
         return;
       }
 
+      console.log(latitude, longitude);
       // Assuming you want to broadcast the coordinates to all connected clients
       io.emit("coordinatesUpdated", { latitude, longitude });
 
