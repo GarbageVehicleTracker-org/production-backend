3b9068350d03c15bc93fe8310d5b09e19cf88c5d
Dhabaldeep <deepdhabal2001@gmail.com>
2024-06-05 01:28:01 +0530

    fix the code

--- controllers/Users.controllers.js
+++ controllers/Users.controllers.js
@@ -53,7 +53,7 @@ class UserController {
 
       const token = user.generateAuthToken();
 
-      res.status(200).json({ message: "Login successful", token });
+      res.status(200).json({ message: "Login successful", token , user});
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Internal server error" });
